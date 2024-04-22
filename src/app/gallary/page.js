cd18858895c8ffd62d2e40bb03ff84fdcaaf77b7
JAHID HASAN <developerjahid@gmail.com>
2024-04-22 17:20:22 +0600

    setup commit

---
+++ src/app/gallary/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gallary = () => {
+    return (
+        <div>
+            <h1 className='text-6xl text-center font-bold'> All Gallary</h1>
+        </div>
+    );
+};
+
+export default Gallary;
